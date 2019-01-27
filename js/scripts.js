91bc92790e12aba9ca339c746187453a8cd79a7f
Porter Savage <theportersavage@gmail.com>
2019-01-26 22:13:10 -0800

    Add initial files for calulator function practice

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+});
+
+var add = function(num1, num2){
+	return num1 + num2;
+};
+
+var number1 = parseInt(prompt("Enter First Number:"));
+var number2 = parseInt(prompt("Enter Second Number:"));
+
+alert(add(number1, number2));
